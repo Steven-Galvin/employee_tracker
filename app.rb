948f9633b42088e33079f7b4c96c0106931fcaa9
Steven Galvin and Tanner Eustice <tanner@>
2017-05-08 13:39:15 -0700

    did it all.

--- app.rb
+++ app.rb
@@ -1,11 +1,82 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra-activerecord'
-require './lib/Employee_Tracker'
+require 'sinatra/activerecord'
+require './lib/division'
+require './lib/employee'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
-get('/') do
+get '/' do
+  @divisions = Division.all
   erb(:index)
+end
+
+# ADD DIVISION
+get '/add_division'do
+  erb(:add_division)
+end
+
+post '/add_division' do
+  name = params['name']
+  Division.new(:name => name).save
+  redirect('/')
+end
+
+
+#DIVISION PAGE
+get '/division/:id' do
+  @division = Division.find(params['id'])
+  @div_employees = @division.employees
+  erb(:division)
+end
+
+delete '/division/:id/delete' do
+  current_division = Division.find(params['id'])
+  current_division.destroy
+  redirect '/'
+end
+
+#DIVISION UPDATE
+get '/division/:id/update' do
+  @division = Division.find(params['id'])
+  erb(:division_update)
+end
+
+patch '/division/:id/update' do
+  current_division = Division.find(params['id'])
+  name = params['name']
+  current_division.update(:name => name)
+  redirect("/division/#{current_division.id}")
+end
+
+#ADD EMPLOYEE
+post '/division/:id/add_employee' do
+  division_id = params['id']
+  name = params['name']
+  Employee.new(:name => name, :division_id => division_id).save
+  redirect("/division/#{division_id}")
+end
+
+#EMPLOYEE PAGE/UPDATE
+get '/division/:division_id/employee/:employee_id' do
+  @division = Division.find(params['division_id'])
+  @employee = Employee.find(params['employee_id'])
+  erb(:employee)
+end
+
+patch '/division/:division_id/employee/:employee_id' do
+  division_id = params['division_id']
+  employee_id = params['employee_id']
+  name = params['name']
+  current_employee = Employee.find(employee_id)
+  current_employee.update(:name => name)
+  redirect ("/division/#{division_id}/employee/#{employee_id}")
+end
+
+delete '/division/:division_id/employee/:employee_id/delete' do
+  division_id = params['division_id']
+  employee = Employee.find(params['employee_id'])
+  employee.destroy
+  redirect("/division/#{division_id}")
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
 describe(Employee) do
-  describe("#divisions") do
+  describe("#division") do
     it("lists the division of a employee") do
       test_division = Division.create({:name => "HR"})
+      puts test_division.id
       test_employee = Employee.create({:name => "Jimmy", :division_id => test_division.id})
       expect(test_employee.division).to eq test_division
     end
